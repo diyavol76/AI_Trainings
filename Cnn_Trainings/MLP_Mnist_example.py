80d3e27f21b5b91d0536e6689db7fa3d323a2ceb
Orhan Erdal <orhan.erdal@esensi.com.tr>
2022-05-13 18:26:29 +0300

    pytorch trainings updated before cnn project

---
+++ Cnn_Trainings/MLP_Mnist_example.py
@@ -0,0 +1,53 @@
+# import libraries
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+# The MNIST datasets are hosted on yann.lecun.com that has moved under CloudFlare protection
+# Run this script to enable the datasets download
+# Reference: https://github.com/pytorch/vision/issues/1938
+
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+# number of subprocesses to use for data loading
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+
+# convert data to torch.FloatTensor
+transform = transforms.ToTensor()
+
+# choose the training and test datasets
+train_data = datasets.MNIST(root='data', train=True,
+                                   download=True, transform=transform)
+test_data = datasets.MNIST(root='data', train=False,
+                                  download=True, transform=transform)
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
+    num_workers=num_workers)
+
+
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+# plot the images in the batch, along with the corresponding labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, int(20/2), idx+1, xticks=[], yticks=[])
+    ax.imshow(np.squeeze(images[idx]), cmap='gray')
+    # print out the correct label for each image
+    # .item() gets the value contained in a Tensor
+    ax.set_title(str(labels[idx].item()))
+
+plt.show()
